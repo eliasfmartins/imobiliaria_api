21f883ea7ba2d1d7792447156b46fe7a192cc95f
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-06-05 21:19:13 -0300

    feat:create search imoveis

---
+++ src/http/controllers/search-imoveis\.ts
@@ -0,0 +1,21 @@
+
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { prisma } from '../../app';
+
+export const searchImoveis = async (
+	request: FastifyRequest,
+	reply: FastifyReply
+) => {
+	try {
+		const imoveis = await prisma.property.findMany();
+		return reply.status(200).send({
+			success: true,
+			count: imoveis.length,
+			data: imoveis,
+		});
+	} catch (e: any) {
+		reply
+			.status(500)
+			.send({ success: false, error: 'Erro ao buscar os imóveis' });
+	}
+};
