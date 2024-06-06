0da122ee5cd9a86a8a4bf7248bfce4bc1e574dd1
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-06-05 21:25:41 -0300

    feat: searchBodySchema

--- src/http/controllers/search-imoveis\.ts
+++ src/http/controllers/search-imoveis\.ts
@@ -1,11 +1,15 @@
-
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { prisma } from '../../app';
+import { z } from 'zod';
 
 export const searchImoveis = async (
 	request: FastifyRequest,
 	reply: FastifyReply
 ) => {
+	const searchBodySchema = z.object({
+		title: z.string(),
+	});
+
 	try {
 		const imoveis = await prisma.property.findMany();
 		return reply.status(200).send({
