2023315640dd245ecba8736af14c8664efa8ed5d
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-06-07 20:49:59 -0300

    feat: habilitando rota de search imoveis

--- src/routes/imoveis.ts
+++ src/routes/imoveis.ts
@@ -5,9 +5,11 @@ import { updateImoveis } from '../http/controllers/update-imoveis';
 import { deleteImoveis } from '../http/controllers/delete-imoveis';
 import { authUser } from '../http/controllers/authenticate';
 import { checkAuth } from '../middlewares/chek-auth';
+import { searchImoveis } from '../http/controllers/search-imoveis';
 
 export async function imoveisRoutes(app: FastifyInstance) {
 	app.get('/', listImoveis);
+	app.post('/search', searchImoveis);
 	app.post('/auth', authUser);
 	app.post('/', { preHandler: [checkAuth] }, register);
 	app.patch('/:id', { preHandler: [checkAuth] }, updateImoveis);
