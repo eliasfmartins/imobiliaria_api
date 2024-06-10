b3f49350731cbc1952513305b695d0a8910ce51c
eliasfmartins <eliasmartinsdev8@gmail.com>
2024-06-10 13:55:22 -0300

    try

--- src/middlewares/chek-auth.ts
+++ src/middlewares/chek-auth.ts
@@ -8,9 +8,10 @@ export const checkAuth = async (request: FastifyRequest, reply: FastifyReply) =>
     try {
             // console.log(request.cookies)
             const token = request.cookies.token;
+            const cookie = request.cookies;
             console.log(token)
             if (!token) {
-                console.log('Deu RUim n auth')
+                console.log('Deu RUim n auth', token, cookie)
                 return reply.status(401).send({ error: 'Unauthorized n achei o token x.x',token });
             }
     
